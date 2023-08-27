42c2e95134964ec17bdd9b803f451f18bedd7b7e
MrCerealGuy <a.zahnleiter@gmx.de>
2023-08-27 18:52:20 +0200

    27082023-1852

--- src/yjak.asm
+++ src/yjak.asm
@@ -1,33 +1,33 @@
 BITS 16
 
 start:
-mov ax, 07C0h		; Set up 4K stack space after this bootloader
-add ax, 288		; (4096 + 512) / 16 bytes per paragraph
+mov ax, 07C0h					; Set up 4K stack space after this bootloader
+add ax, 288						; (4096 + 512) / 16 bytes per paragraph
 mov ss, ax
 mov sp, 4096
 
-mov ax, 07C0h		; Set data segment to where we're loaded
+mov ax, 07C0h					; Set data segment to where we're loaded
 mov ds, ax
 
 call cls
 
-mov si, text_string	; Put string position into SI
-call print_string	; Call our string-printing routine
+mov si, text_string				; Put string position into SI
+call print_string				; Call our string-printing routine
 
-jmp $			; Jump here - infinite loop!
+jmp $							; Jump here - infinite loop!
 
 
 text_string db 'Hello World from YJAK-OS!', 0
 
 
-print_string:	; Routine: output string in SI to screen
-mov ah, 0Eh		; int 10h 'print char' function
+print_string:					; Routine: output string in SI to screen
+mov ah, 0Eh						; int 10h 'print char' function
 
 .repeat:
-lodsb			; Get character from string
+lodsb							; Get character from string
 cmp al, 0
-je .done		; If char is zero, end of string
-int 10h			; Otherwise, print it
+je .done						; If char is zero, end of string
+int 10h							; Otherwise, print it
 jmp .repeat
 
 .done:
@@ -36,10 +36,10 @@ ret
 cls:
 pusha
 mov ah, 0x00
-mov al, 0x03  ; text mode 80x25 16 colours
+mov al, 0x03  					; text mode 80x25 16 colours
 int 0x10
 popa
 ret
 
-times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
-dw 0xAA55				; The standard PC boot signature
+times 510-($-$$) db 0			; Pad remainder of boot sector with 0s
+dw 0xAA55						; The standard PC boot signature
